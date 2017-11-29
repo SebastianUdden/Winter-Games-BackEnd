3b9b5b0c07945583931cb7a0f89fab5d4c0197f5
Sebastian Uddén <sebastian.udden@eurostep.com>
2017-11-29 11:37:26 +0100

    register, login and other user functionality fixed

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var path = require('path');
 var open = require('open');
 var port = process.env.PORT || 3000;
 var app = express();
+var apiRoutes = express.Router(); 
 
 var bodyParser = require('body-parser');
-var logger = require('./logger');
+// var logger = require('./logger');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(logger);
+// app.use(logger);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", 'http://localhost:4200'); //<-- you can change this with a specific url like http://localhost:4200
@@ -45,10 +46,10 @@ db.once('open', function() {
 //////////////////////////////////////////////////////////////////////////////
 var userSchema = mongoose.Schema({
     id: { type: Number },
-    userName: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     score: { type: Number },
     level: { type: String },
@@ -60,48 +61,65 @@ var User = mongoose.model('User', userSchema);
 User.find(function (err, users) {
     if (err) return console.error(err);
     users.forEach(user => {
-        console.log(user.userName + ', ' + user.score);
+        console.log(user.username + ', ' + user.score);
+        console.log(user);
     });
     // console.log(users);
 });
 
-app.get('/', function(req, res) {    
+
+////////////////////
+// JSON Web Tokens
+////////////////////////////////////////////////////////////////////////////////
+// var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+// var config = {
+//     'secret': 'ILOVEWINTERGAM3S'
+// };
+// app.set('superSecret', config.secret); // secret variable
+
+///////////
+// Morgan
+////////////////////////////////////////////////////////////////////////////////
+var morgan      = require('morgan');
+app.use(morgan('dev'));
+
+//////////////
+// API Calls
+////////////////////////////////////////////////////////////////////////////////
+apiRoutes.get('/', function(req, res) {    
     res.redirect('/users');
     // res.send('Hello Universe!');
     // res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-app.get('/users', function(req, res) {    
+apiRoutes.get('/users', function(req, res) {    
     User.find(function (err, users) {
         if (err) { return res.send(err); }
         res.send(users);
     });
 });
-app.get('/users/:id', function(req, res) {    
-    User.findOne({ id: req.params.id}, function(err, user) {
+apiRoutes.get('/users/:username', function(req, res) {    
+    User.findOne({ username: req.params.username}, function(err, user) {
         if (err) { res.send(err); }
         res.json(user);
     });
 });
-
-app.post('/users', function(req, res) {
+apiRoutes.post('/users', function(req, res) {
     User.create(req.body, function(err, user) {
         if (err) { res.send(err); }
         res.json(user);
         console.log(user);
     });
 });
-
-app.put('/users/:id', function(req, res) {
-    User.findOneAndUpdate({ id: req.params.id }, { $set: req.body }, {new: true}, function(err, user) {
+apiRoutes.put('/users/:username', function(req, res) {
+    User.findOneAndUpdate({ username: req.params.username }, { $set: req.body }, {new: true}, function(err, user) {
         if (err) { res.send(err); }
         res.json(user);
         console.log(user);
     });
 });
-
-app.delete('/users/:id', function(req, res) {
+apiRoutes.delete('/users/:username', function(req, res) {
     User.remove({
-        id: req.params.id
+        username: req.params.username
     }, function(err, task) {
     if (err) { res.send(err); }
         res.json({ message: 'User ' + req.params.id + ' successfully deleted' });
@@ -111,6 +129,7 @@ app.delete('/users/:id', function(req, res) {
 ////////////////
 // Listen
 ///////////////////////////////////////////////////////////////////////////////
+app.use('/api', apiRoutes);
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
@@ -119,3 +138,77 @@ app.listen(port, function(err) {
         // open('http://localhost:' + port);
     }
 });
+
+
+
+
+
+
+
+
+
+///////////////////
+// Authentication
+////////////////////////////////////////////////////////////////////////////////
+// apiRoutes.post('/authenticate', function(req, res) {
+//     // find the user
+//     User.findOne({
+//         userName: req.body.userName
+//     }, function(err, user) {
+//         if (err) { throw err; }
+
+//         if (!user) {
+//             res.json({ success: false, message: 'Authentication failed. User not found.' });
+//         } else if (user) {
+//             // check if password matches
+//             if (user.password != req.body.password) {
+//                 res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+//             } else {                
+//                 // if user is found and password is right
+//                 // create a token with only our given payload
+//                 // we don't want to pass in the entire user since that has the password
+//                 const payload = {
+//                     admin: user.admin 
+//                 };
+                
+//                 var token = jwt.sign(payload, app.get('superSecret'), {
+//                     expiresIn : 60*60*24 // expires in 24 hours
+//                 });
+
+//                 // return the information including token as JSON
+//                 res.json({
+//                     success: true,
+//                     message: 'Enjoy your token!',
+//                     token: token
+//                 });
+//             }   
+//         }
+//     });
+// });
+
+// // route middleware to verify a token
+// apiRoutes.use(function(req, res, next) {
+//     // check header or url parameters or post parameters for token
+//     var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+//     // decode token
+//     if (token) {
+//         // verifies secret and checks exp
+//         jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+//             if (err) {
+//                 return res.json({ success: false, message: 'Failed to authenticate token.' });    
+//             } else {
+//                 // if everything is good, save to request for use in other routes
+//                 req.decoded = decoded;    
+//                 next();
+//             }
+//         });
+//     } else {
+//         // if there is no token
+//         // return an error
+//         return res.status(403).send({ 
+//             success: false, 
+//             message: 'No token provided.' 
+//         });
+//     }
+// });
